e7b6c54ccb86386f7fe7733ca9cff476d322734b
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2024-07-08 21:56:29 +0600

    first commit

---
+++ src/components/About/About.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt unde
+        molestias, ut recusandae praesentium nemo amet sapiente delectus quidem
+        quibusdam id non, culpa obcaecati voluptatum assumenda, ipsa saepe
+        perferendis labore.
+      </h1>
+    </div>
+  );
+};
+
+export default About;
