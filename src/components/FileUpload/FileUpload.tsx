e085ad26b8fe4595ea38080875247ad9b656aec8
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2024-07-13 03:31:45 +0600

    api added

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -78,7 +78,7 @@ export default function FileUpload() {
       const data = new FormData();
       data.set("file", file);
       const res = await fetch(
-        "http://sophisticated-file-management-backend.vercel.app/api/upload",
+        "https://sophisticated-file-management-backend.vercel.app/api/upload",
         {
           method: "POST",
           body: data,
